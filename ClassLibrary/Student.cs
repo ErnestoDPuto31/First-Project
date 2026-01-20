cdc4bbad35f0049fdf2d8b408e89c08d5085ffc7
ErnestoDPuto31 <ernest.agalo31@gmail.com>
2026-01-20 08:17:56 +0800

    2nd Commit

---
+++ ClassLibrary/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
